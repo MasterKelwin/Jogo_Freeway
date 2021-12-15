9879dc84f1ad18879d9585261617162abab77ce1
MasterKelwin <mkelwin@outlook.com>
2021-12-15 13:12:43 -0300

    loop adicionado

--- sketch.js
+++ sketch.js
@@ -11,17 +11,17 @@ let wAtor = 30
 let hAtor = 30
 
 //variáveis do carro1
-let xCarro1 = 420
+let xCarro1 = 600
 let yCarro1 = 40
 let wCarro = 60
 let hCarro = 40
 
 //variáveis do carro2
-let xCarro2 = 420
+let xCarro2 = 600
 let yCarro2 = 96
 
 //variaveis do carro3
-let xCarro3 = 420
+let xCarro3 = 600
 let yCarro3 = 150
 
 
@@ -45,6 +45,8 @@ function draw() {
   mostraCarro3();
   movimentaAtor();
   movimentaCarro();
+  loopCarro();
+
 }
 
 function mostraAtor () {
@@ -64,22 +66,29 @@ function mostraCarro1 () {
   image(carro1, xCarro1, yCarro1, wCarro, hCarro);
 }
 
-function movimentaCarro1 () {
-  xCarro1 -= 4
-}
-
 function mostraCarro2 () {
   image(carro2, xCarro2, yCarro2, wCarro, hCarro);
-}
-
-function movimentaCarro2 () {
-  xCarro2 -= 5
 }
 
 function mostraCarro3 () {
   image(carro3, xCarro3, yCarro3, wCarro, hCarro);
 }
 
-function movimentaCarro3 () {
+
+function movimentaCarro () {
+  xCarro1 -= 4 //velocidades
+  xCarro2 -= 5
   xCarro3 -= 3
+}
+
+function loopCarro () {
+  if (xCarro1 < -80) {
+    xCarro1 = 600
+  }
+    if (xCarro2 < -80) {
+    xCarro2 = 600
+  }
+    if (xCarro3 < -80) {
+    xCarro3 = 600
+  }
 }
